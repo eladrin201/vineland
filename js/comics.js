f8dcf59f7f2c287a440cc65f1e569e25831aca63
Allison B <unreasonablyniceee@gmail.com>
2014-05-06 13:12:31 -0500

    Updated website layout and added sample comic

--- js/comics.js
+++ js/comics.js
@@ -2,21 +2,21 @@
 
 angular.module("micro").constant("COMICS", {
 	arcs: [
-		{title: "Default Arc", chapters:[
-			{title: "Default Chapter", pages:[
-				{title: "Default Page", image: "comics/1.jpg"},
-				{title: "Hello", image:"comics/2.jpg"},
-				{title: "World", image:"comics/3.jpg"}
+		{title: "Vineland", chapters:[ //Arc name
+			{title: "Sample Chapter", pages:[ //chapter name
+				{title: "Page 7", image:"comic/Dennou_Alice/7.jpg"},
+				{title: "Page 6", image:"comic/Dennou_Alice/6.jpg"},
+				{title: "Page 5", image:"comic/Dennou_Alice/5.jpg"},
+				{title: "Page 4", image:"comic/Dennou_Alice/4.jpg"},
+				{title: "Page 3", image:"comic/Dennou_Alice/3.jpg"},
+				{title: "Page 2", image:"comic/Dennou_Alice/2.jpg"}, //pages, put the most recent on the top!!
+				{title: "Page 1", image:"comic/Dennou_Alice/1.jpg"}
 			]},
-			{title: "That other chapter", pages:[
+			{title: "That other chapter", pages:[ //chapter name
 				{title: "Sup", image: "comics/4.jpg"},
 				{title: "Dawg", image: "comics/5.jpg"}
 			]}
 		]},
-		{title: "Other Arc", chapters:[
-			{title: "Chapter Fuckoff", pages: [
-				{title: "Yo", image: "comics/3.jpg"}
-			]}
-		]}
+
 	]
 });
